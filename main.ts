91ff1542b51930c87d75a67804ff3ab4c529408f
littlestriker132 <norwin.wang@warriorlife.net>
2026-07-08 09:56:14 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,16 @@ namespace StatusBarKind {
 }
 scene.setBackgroundColor(5)
 let LoadingProgress = 0
+let Title = fancyText.create("Backrooms")
 let statusbar = statusbars.create(100, 4, StatusBarKind.Loading)
 statusbar.setPosition(80, 100)
+statusbar.setColor(15, 1)
 for (let index = 0; index < 10; index++) {
     LoadingProgress += 10
     statusbar.value = LoadingProgress
     pause(randint(100, 800))
 }
+sprites.destroy(statusbar)
+game.onUpdateInterval(500, function () {
+	
+})

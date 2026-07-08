b4943f9ef82e0f169407b8881370d251cc7f0150
littlestriker132 <norwin.wang@warriorlife.net>
2026-07-08 09:41:23 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+namespace StatusBarKind {
+    export const Loading = StatusBarKind.create()
+}
+scene.setBackgroundColor(5)
+let LoadingProgress = 0
+let statusbar = statusbars.create(100, 4, StatusBarKind.Loading)
+statusbar.setPosition(80, 100)
+for (let index = 0; index < 10; index++) {
+    LoadingProgress += 10
+    statusbar.value = LoadingProgress
+    pause(randint(100, 800))
+}
